e7b0d2ee8d83ca81a59b910b7650720e2b6a37d5
Ronny de Winter <ronny.de.winter@opticon.com>
2024-10-09 16:24:46 +0200

    Updated reading of barcode sections and removed unused files and some cosmetics

---
+++ projects/applications/HelloWorld/Output/Debug OPN2500/Obj/OPN_HelloWorld_Appl/nrf_sections.d
@@ -0,0 +1,44 @@
+Output/Debug\ OPN2500/Obj/OPN_HelloWorld_Appl/nrf_sections.o: \
+ C:\OPN_NORDIC\git\opn2500_opn6000_sdk\SDK\softdevice\common\nrf_sections.c \
+ C:\OPN_NORDIC\git\opn2500_opn6000_sdk\SDK\softdevice\common\nrf_sdh.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/stdbool.h \
+ ../../../SDK/include/sdk_config.h ../../../SDK/include/sdk_errors.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/__SEGGER_RTL_Arm_Conf.h \
+ ../../../SDK/softdevice/s113/headers/nrf_error.h \
+ ../../../SDK/include/nrf_section_iter.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.18/include/stddef.h \
+ ../../../SDK/include/nrf_section.h ../../../SDK/include/nordic_common.h \
+ ../../../SDK/include/app_util.h \
+ ../../../SDK/include/compiler_abstraction.h ../../../SDK/mdk/nrf.h \
+ ../../../SDK/mdk/nrf52840.h ../../../SDK/toolchain/cmsis/core_cm4.h \
+ ../../../SDK/toolchain/cmsis/cmsis_version.h \
+ ../../../SDK/toolchain/cmsis/cmsis_compiler.h \
+ ../../../SDK/toolchain/cmsis/cmsis_gcc.h \
+ ../../../SDK/toolchain/cmsis/mpu_armv7.h \
+ ../../../SDK/mdk/system_nrf52840.h ../../../SDK/mdk/nrf52840_bitfields.h \
+ ../../../SDK/mdk/nrf51_to_nrf52840.h \
+ ../../../SDK/mdk/nrf52_to_nrf52840.h \
+ ../../../SDK/mdk/compiler_abstraction.h \
+ ../../../SDK/softdevice/s113/headers/nrf52/nrf_mbr.h \
+ ../../../SDK/softdevice/s113/headers/nrf_svc.h \
+ C:\OPN_NORDIC\git\opn2500_opn6000_sdk\SDK\softdevice\common\nrf_sdh_soc.h \
+ ../../../SDK/softdevice/s113/headers/nrf_soc.h \
+ ../../../SDK/softdevice/s113/headers/nrf_svc.h \
+ ../../../SDK/softdevice/s113/headers/nrf_error.h \
+ ../../../SDK/softdevice/s113/headers/nrf_error_soc.h \
+ C:\OPN_NORDIC\git\opn2500_opn6000_sdk\SDK\softdevice\common\nrf_sdh_ble.h \
+ ../../../SDK/include/app_util.h \
+ ../../../SDK/softdevice/s113/headers/ble.h \
+ ../../../SDK/softdevice/s113/headers/ble_err.h \
+ ../../../SDK/softdevice/s113/headers/ble_gap.h \
+ ../../../SDK/softdevice/s113/headers/ble_hci.h \
+ ../../../SDK/softdevice/s113/headers/ble_ranges.h \
+ ../../../SDK/softdevice/s113/headers/ble_types.h \
+ ../../../SDK/softdevice/s113/headers/ble_l2cap.h \
+ ../../../SDK/softdevice/s113/headers/ble_gatt.h \
+ ../../../SDK/softdevice/s113/headers/ble_gattc.h \
+ ../../../SDK/softdevice/s113/headers/ble_gatts.h
---
+++ projects/applications/HelloWorld/version.c
@@ -0,0 +1,5 @@
+// !!!!AUTO GENERATED FILE by prebuild.bat!!!! 
+#include "lib.h" 
+#ifdef APP_VERSION 
+APP_VERSION_SECT const char APPversionstring[16] = APP_VERSION; 
+#endif // APP_VERSION 
